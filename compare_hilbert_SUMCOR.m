c02e9173005946b6e8effd70e19eb29b255eede6
sbp894 <sbp894@users.noreply.github.com>
2017-07-11 15:41:17 -0400

    Added compare_hilbert_SUMCOR.

--- compare_hilbert_SUMCOR.m
+++ compare_hilbert_SUMCOR.m
@@ -1,13 +1,16 @@
 %%
-% Created by SP
-% To see the effects of adaptation on SUMCOR PSD. Adaptation acts as a low
-% pass filter. So adaptation should results in higher power in lower
-% modulation frequencies. We are getting higher power using neural sEPSM in
-% 1 and 2 Hz modulation frequency bands.
+% Created by SP (11 July, 2017)
+% To see if results based on Hilbert Transforma and SUMCOR of neanrate
+% yield diffent results. 
+% Testing because: 
+%         - Christoph's model (based on Hilbert) says in SAM noise, SNRenv
+%         mostly come from higher modulation frequencies. 
+%         - n_sEPSM's model (based on SUMCOR) says in SAM noise, SNRenv
+%         mostly come from 1 and 2 Hz modulation frequencies. 
 
 %%
 clear;
-close all;
+% close all;
 clc;
 
 addpath('mexsource\');
@@ -15,13 +18,15 @@ addpath('mexsource\');
 %%
 all_nReps=5;
 all_SRtype=1:3;
-All_stims={{'Stimuli\stimSetStationary\Stim3dB_SN_P.wav', 'Stimuli\stimSetStationary\Stim3dB_N_P.wav'}, {'Stimuli\stimSetFluctuating\Stim3dB_SN_P.wav', 'Stimuli\stimSetFluctuating\Stim3dB_N_P.wav'}};
+SNR=-6;
+All_stims={{['Stimuli\stimSetStationary\Stim' num2str(SNR) 'dB_SN_P.wav'], ['Stimuli\stimSetStationary\Stim' num2str(SNR) 'dB_N_P.wav']}, ...
+    {['Stimuli\stimSetFluctuating\Stim' num2str(SNR) 'dB_SN_P.wav'], ['Stimuli\stimSetFluctuating\Stim' num2str(SNR) 'dB_N_P.wav']}};
 for stimVar=1:length(All_stims)
     stimNames=All_stims{stimVar};
     srLetters='LMH';
     
     %%
-    AN.CF_Hz=8e3;
+    AN.CF_Hz=.4e3;
     AN.AN_Fs_Hz=100e3;
     AN.Cohc=1;
     AN.Cihc=1;
@@ -39,14 +44,16 @@ for stimVar=1:length(All_stims)
     params.DELAYbinwidth=50e-6;
     params.plotYes=0;
     params.useNeural=0;
+    params.useNeural=0;
     
     %%
-    lw=2;
+    hFig.lw=2;
     hFig.h_pMod=234;
     hFig.h_SC=746;
     hFig.SNRenv=1;
     hFig.linestyles={'-', '--', '-.'};
     figure(stimVar);
+    clf;
     co=get(gca, 'colororder');
     
     %%
@@ -56,15 +63,18 @@ for stimVar=1:length(All_stims)
     
     if params.useNeural
         legstr=cell(length(all_nReps)+2,1);
+        legstr2=cell(2*(length(all_nReps)+2),1);
     else
         legstr=cell(2,1);
+        legstr2=cell(4,1);
     end
-    legstr{1}='tAdapt';
-    legstr{2}='tUnad';
+    legstr(1:2)={'tSUMCOR', 'tHilbert'};
+    legstr2(1:4)={'SN-tSUMCOR', 'SN-tHilbert','N-tSUMCOR', 'N-tHilbert'};
     if params.useNeural
         for nRepVar=1:length(all_nReps)
             legstr{nRepVar+2}=sprintf('nRep=%i', all_nReps(nRepVar));
-            legstr2
+            legstr2{2*(nRepVar+2)-1}=sprintf('SN-nRep=%i', all_nReps(nRepVar));
+            legstr2{2*(nRepVar+2)}=sprintf('N-nRep=%i', all_nReps(nRepVar));
         end
     end
     %% Main loop
@@ -77,15 +87,15 @@ for stimVar=1:length(All_stims)
             
             % SN
             params.StimName=stimNames{1};
-            [n_PowerMod_SN, t_ad_PowerMod_SN, t_ua_PowerMod_SN]=fun_getpMOD(params, AN, hFig);
+            [n_PowerMod_SN, t_ad_PowerMod_SN, t_hilbert_PowerMod]=fun_getpMOD(params, AN, hFig);
             subplot(length(all_SRtype),2,2*srVar-1);
             hold on;
             if nRepVar==1
-                plot(t_ad_PowerMod_SN, [ hFig.linestyles{1} 's'], 'color', co(1,:), 'linewidth', lw);
-                plot(t_ua_PowerMod_SN, [ hFig.linestyles{1} 's'], 'color', co(2,:), 'linewidth', lw);
+                plot(t_ad_PowerMod_SN, [ hFig.linestyles{1} 's'], 'color', co(1,:), 'linewidth', hFig.lw);
+                plot(t_hilbert_PowerMod, [ hFig.linestyles{1} 's'], 'color', co(2,:), 'linewidth', hFig.lw);
             end
             if params.useNeural
-                plot(n_PowerMod_SN, [ hFig.linestyles{1} 'o'], 'color', co(2+nRepVar,:), 'linewidth', lw);
+                plot(n_PowerMod_SN, [ hFig.linestyles{1} 'o'], 'color', co(2+nRepVar,:), 'linewidth', hFig.lw);
             end
             % N
             params.StimName=stimNames{2};
@@ -93,25 +103,27 @@ for stimVar=1:length(All_stims)
             subplot(length(all_SRtype),2,2*srVar-1);
             hold on;
             if nRepVar==1
-                plot(t_ad_PowerMod_N, [ hFig.linestyles{2} 'd'], 'color', co(1,:), 'linewidth', lw);
-                plot(t_ua_PowerMod_N, [ hFig.linestyles{2} 'd'], 'color', co(2,:), 'linewidth', lw);
+                plot(t_ad_PowerMod_N, [ hFig.linestyles{2} 'd'], 'color', co(1,:), 'linewidth', hFig.lw);
+                plot(t_ua_PowerMod_N, [ hFig.linestyles{2} 'd'], 'color', co(2,:), 'linewidth', hFig.lw);
             end
             if params.useNeural
-                plot(n_PowerMod_N, [ hFig.linestyles{2} 'o'], 'color', co(2+nRepVar,:), 'linewidth', lw);
+                plot(n_PowerMod_N, [ hFig.linestyles{2} 'o'], 'color', co(2+nRepVar,:), 'linewidth', hFig.lw);
             end
             
             set(gca, 'XTick', 1:7, 'xticklabel',  params.strModFreqs);
             
             %% SNRenv calculations
-            nSe=(n_PowerMod_SN-n_PowerMod_N)./n_PowerMod_N;
-            nSe(nSe<0)=0;
-            n_SNRenv(srVar, nRepVar, :)=nSe;
+            if params.useNeural
+                nSe=(n_PowerMod_SN-n_PowerMod_N)./n_PowerMod_N;
+                nSe(nSe<0)=0;
+                n_SNRenv(srVar, nRepVar, :)=nSe;
+            end
             
             t_ad_Se=(t_ad_PowerMod_SN-t_ad_PowerMod_N)./t_ad_PowerMod_N;
             t_ad_Se(t_ad_Se<0)=0;
             t_ad_SNRenv(srVar, nRepVar, :)=t_ad_Se;
             
-            t_ua_Se=(t_ua_PowerMod_SN-t_ua_PowerMod_N)./t_ua_PowerMod_N;
+            t_ua_Se=(t_hilbert_PowerMod-t_ua_PowerMod_N)./t_ua_PowerMod_N;
             t_ua_Se(t_ua_Se<0)=0;
             t_ua_SNRenv(srVar, nRepVar, :)=t_ua_Se;
             
@@ -119,18 +131,18 @@ for stimVar=1:length(All_stims)
             subplot(length(all_SRtype),2,2*srVar);
             hold on;
             if nRepVar==1
-                plot(t_ad_Se, '-s', 'linewidth', lw);
-                plot(t_ua_Se, '-s', 'linewidth', lw);
+                plot(t_ad_Se, '-s', 'linewidth', hFig.lw);
+                plot(t_ua_Se, '-s', 'linewidth', hFig.lw);
             end
             if params.useNeural
-                plot(nSe, '-o', 'linewidth', lw);
+                plot(nSe, '-o', 'linewidth', hFig.lw);
             end
         end
-        title(sprintf('%sSR, SNRenv=%.2f (t-Ad) and %.2f (t-unad)', srLetters(params.SRtype), nansum(t_ad_Se.^2), nansum(t_ua_Se.^2)));
+        title(sprintf('%sSR, SNRenv=%.2f (t-SUMCOR) and %.2f (t-Hilbert)', srLetters(params.SRtype), nansum(t_ad_Se.^2), nansum(t_ua_Se.^2)));
         set(gca, 'XTick', 1:7, 'xticklabel',  params.strModFreqs);
     end
     
     legend(legstr, 'location', 'best');
     subplot(length(all_SRtype),2,2*srVar-1);
-    legend(legstr2);
+    legend(legstr2, 'location', 'best');
 end
